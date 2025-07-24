b122096443b41f1eed98bc9f4c8afb4208df89f7
sykharawnte <kharwanteshubhada07@gmail.com>
2025-07-24 15:17:35 +0000

    add all

---
+++ scripts/build_and_push.sh
@@ -0,0 +1,14 @@
+# scripts/build_and_push.sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+GIT_COMMIT=$(git rev-parse --short HEAD)
+IMAGE="shubhadak/myapp:$GIT_COMMIT"
+
+echo "[+] Building Docker image..."
+docker build -t $IMAGE .
+
+echo "[+] Pushing Docker image to DockerHub..."
+docker push $IMAGE
+
+ssh -i ~/.ssh/one.pem ubuntu@52.66.209.208
